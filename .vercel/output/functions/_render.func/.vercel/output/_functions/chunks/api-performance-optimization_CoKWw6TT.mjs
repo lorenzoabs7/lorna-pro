0823897a07c2fd8a62f42252e34ea1cc563e7bd4
Lorenzo Blanda <lorenzoblanda17@gmail.com>
2026-03-02 19:50:03 -0300

    Remove .vercel build artifacts from git

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/api-performance-optimization_CoKWw6TT.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./api-performance-optimization_C8XnC1nW.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/cicd-modernization_2XlN3ZzE.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./cicd-modernization_C8km4zbN.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
